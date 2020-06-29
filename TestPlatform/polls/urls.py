101d9b5e4b298ea8e5cc74ed1a11ed9619e5e355
3434-v <liangpeng@drep.org>
2020-06-29 22:39:27 +0800

    add msg

--- TestPlatform/polls/urls.py
+++ TestPlatform/polls/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
-    path('<int:question_id>/',views.detail,name="detail"),
-    path('<int:question_id>/results/',views.results,name="result"),
-    path('<int:question_id>/vote',views.vote,name="vote")
+    path('', views.home, name='home'),
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/results/', views.results, name="result"),
+    path('<int:question_id>/vote', views.vote, name="vote"),
+    path('index', views.index, name="index"),
+
 ]
--- TestPlatform/polls/views.py
+++ TestPlatform/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 # from polls.models import Question
-from polls.models import Choice, Question
+from polls.models import Question
 
 def home(request):
     return HttpResponse("python-django 3.0")
@@ -11,6 +11,7 @@ def detail(request, question_id):
     return HttpResponse("detailmessage:{}".format(question_id))
 
 def results(request, question_id):
+    print('-------------')
     response = "resultsmessage {}"
     return HttpResponse(response.format(question_id))
 
@@ -18,5 +19,9 @@ def vote(request, question_id):
     return HttpResponse("votemessage:{}".format(question_id))
 
 def index(request):
-    question_list = Question.objects.order_by('-pub_data')[:5]
+    question_list = Question.objects.all()
+    print('-------------')
+    print(question_list)
+    print('-------------')
     output = ', '.join([q.question_text for q in question_list])
+    return HttpResponse(output)
